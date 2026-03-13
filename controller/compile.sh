0592da7c434b1f98a1c0c0bb912d6f5ff23072c6
Thomas Harrison <69117191+ThomasH06@users.noreply.github.com>
2026-03-13 11:55:57 -0700

    Delete controller directory

--- controller/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-rm -rf build
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=xc32-toolchain.cmake
-cmake --build build
--- controller/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-# Not really sure why this is necessary, but it is
-export DYLD_LIBRARY_PATH=/usr/local/lib:$DYLD_LIBRARY_PATH
-pro32 -p -v build/controller.hex
